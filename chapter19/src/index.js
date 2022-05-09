5ead5ca0e08586dc3c78e1054fab568a18fa2beb
jonghyun paik <paikga3@etoursoft.co.kr>
2022-05-09 17:37:54 +0900

    chapter

--- chapter19/src/index.js
+++ chapter19/src/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
-import rootReducer from './modules';
 import reportWebVitals from './reportWebVitals';
-
-const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
